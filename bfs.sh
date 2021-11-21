26ea79b6065e5ef8ebaaf8e6831cb880a0fe4b90
Bob96341 <64376126+Bob96341@users.noreply.github.com>
2021-11-21 23:16:28 +0100

    Update bfs.sh

--- bfs.sh
+++ bfs.sh
@@ -3,15 +3,39 @@
 clear ; ls -a | lolcat
 while read line
 do
-	if [ "$line" = "exit" ]
-	then
-		clear ; break
-	fi
-	if clear ; cd "$line" 
-	then
-		ls -a | lolcat
-	else
-		clear ; ls -a | lolcat
-		echo "Directory does not exist"
-	fi
+	case $line in
+		"exit")
+			clear ; break
+			;;
+		"mkdir")
+			clear ; ls -a | lolcat 
+			echo "Name the directory: " 
+			read namae 
+			mkdir "$namae" 
+			clear ; ls -a | lolcat
+			;;
+		"touch")
+			clear ; ls -a | lolcat 
+			echo "Name the file: " 
+			read namae 
+			touch "$namae" 
+			clear ; ls -a | lolcat
+			;;
+		"rm")
+			clear ; ls -a | lolcat 
+			echo "Remove which file? " 
+			read namae
+			rm "$namae" ; rm -d "$namae" 
+			clear ; ls -a | lolcat
+			;;
+		*)
+			if clear ; cd "$line"
+			then
+				ls -a | lolcat
+			else
+				clear ; ls -a | lolcat
+				echo "Error: Wrong Directory"
+			fi
+			;;
+	esac
 done
